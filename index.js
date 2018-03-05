81ab97718da488c99c6b6cef9823f4cc73fb4144
nishanth <nishanth@codeastra.com>
2018-03-05 13:09:22 +0530

    port has been changed to 8080

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
+
+console.log('test1');
+console.log(process.env.PORT);
+console.log(port);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  console.log('a user connected');
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
